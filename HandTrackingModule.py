bb38c84d0443f314bfa11c015bcde6a60e332ae7
the-one-rvs <vaibhavsarswat142005@gmail.com>
2023-12-30 18:07:25 +0530

    Created Hand Gesture Volume Control Project using OpenCV

---
+++ HandTrackingModule.py
@@ -0,0 +1,67 @@
+import cv2 as cv
+import mediapipe as mp 
+import time
+
+class handDetector():
+    def __init__ (self, mode=False, complexity = 1, maxHands = 2, detectionCon = 0.5, trackCon = 0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.complexity = complexity
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.complexity, self.detectionCon, self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
+
+    def findHands(self, frame, draw=True):
+        frame_rgb = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+        self.results = self.hands.process(frame_rgb)
+        
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(frame, handLms, self.mpHands.HAND_CONNECTIONS)
+        return frame
+    def findPosition(self, img, handNo=0, draw=True):
+
+        lmlist=[]
+        if self.results.multi_hand_landmarks:
+
+            myHand = self.results.multi_hand_landmarks[handNo]
+
+            for id, lm in enumerate(myHand.landmark):
+                h,w,c = img.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                lmlist.append([id, cx,cy])
+                if draw:
+                    cv.circle(img, (cx,cy), 7 ,(0,140,255), cv.FILLED)
+        return lmlist
+
+
+def main():
+    pTime = 0
+    cTime = 0
+    capture = cv.VideoCapture(0)
+    detector = handDetector()
+    while True:
+        isTrue, img = capture.read()
+        img = detector.findHands(img)
+        lmList = detector.findPosition(img)
+
+        cTime = time.time()
+        fps = 1/(cTime - pTime)
+        pTime = cTime
+
+        cv.putText(img, str(int(fps)), (10,70), cv.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
+
+        cv.imshow('Camera',img)
+
+        if cv.waitKey(20) & 0xFF==ord('d'):
+            break
+
+    capture.release()
+    cv.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
---
+++ Volume_Gesture_Control.py
@@ -0,0 +1,87 @@
+import cv2 as cv
+import time
+import numpy as np 
+import HandTrackingModule as htm 
+import math
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+
+
+
+wcam, hcam = 640, 480
+
+
+cap = cv.VideoCapture(0) 
+cap.set(3, wcam)
+cap.set(4, wcam)
+
+cTime=0
+pTime=0
+
+detector = htm.handDetector(detectionCon= 0.7)
+
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(
+    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = interface.QueryInterface(IAudioEndpointVolume)
+volume.GetMute()
+volRange = volume.GetVolumeRange()
+
+minVol = volRange[0]
+maxVol = volRange[1]
+vol= minVol
+volBar= 400
+volPer = 0
+
+while True:
+    isTrue, img = cap.read()
+
+    img = detector.findHands(img)
+    lmlist = detector.findPosition(img)
+    if len(lmlist) != 0:
+        
+        x1, y1= lmlist[4][1], lmlist[4][2]
+        x2, y2= lmlist[8][1], lmlist[8][2]
+
+        cx,cy = (x1+x2)//2, (y1+y2)//2
+
+        cv.circle(img, (x1,y1), 11, (100,50,170), cv.FILLED)
+        cv.circle(img, (x2,y2), 11, (100,50,170), cv.FILLED)
+        
+        cv.circle(img, (cx,cy), 11, (100,50,170), cv.FILLED)
+
+        length = math.hypot(x2-x1, y2-y1)
+        
+        
+        vol = np.interp(length, [30,200], [minVol , maxVol])
+        volBar = np.interp(length, [30,200], [400,150])
+        
+        volPer = (1 + vol/ 65) * 100
+
+
+        print(int(length), vol)
+        volume.SetMasterVolumeLevel(vol, None)
+
+
+        if length<50:
+            cv.circle(img, (cx,cy), 11, (0,255,0), cv.FILLED)
+        
+        cv.rectangle(img, (50,150), (85,400), (150,250,100), 3)
+        cv.rectangle(img, (50,int(volBar)), (85,400), (150,250,100), cv.FILLED)
+        cv.putText(img, f'{int(volPer)} %', (40,450), cv.FONT_HERSHEY_COMPLEX, 1, (150,250,100), 3)
+
+
+    cTime = time.time()
+    fps = 1/(cTime - pTime)
+    pTime = cTime
+
+    cv.putText(img, f'FPS: {int(fps)}', (40,50), cv.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 3)
+
+    cv.imshow('Camera',img)
+
+
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+
+cap.release()
+cv.destroyAllWindows()
